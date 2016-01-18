0838573286ef984defd0e797d546f68d9b4522d5
DevNock <DevNock@gmail.com>
2016-01-18 16:58:00 +0300

    hide title and action bar

--- app/src/main/java/com/google/criminalintent/CrimeCameraActivity.java
+++ app/src/main/java/com/google/criminalintent/CrimeCameraActivity.java
@@ -1,11 +1,22 @@
 package com.google.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * Created by Sergey on 16.01.2016.
  */
 public class CrimeCameraActivity extends SingleFragmentActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.CameraTheme);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);       // Hide action panel and other OC view
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     protected Fragment createFragment() {
         return new CrimeCameraFragment();
